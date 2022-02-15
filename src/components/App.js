524d7500c4d3ca1141f86996ab4493e6bf120ea4
Iuliia El Sammak <juliag875@gmail.com>
2022-02-14 22:02:49 -0500

    css adde, image added

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <h2><i style={{color: "yellow"}}>Enjoy 3 FREE readings + 70% off your first session!</i></h2>
+      <h2><i>Enjoy 3 FREE readings + 70% off your first session!</i></h2>
+      <img src="https://media.istockphoto.com/vectors/beauty-lotus-logo-vector-design-illustration-vector-id1224093652?k=20&m=1224093652&s=170667a&w=0&h=Jqhwgn3piTenNuqwvtMkeJH6qlp1tfTjPjmUOKFH1hE="/>
       <Switch>
         <Route path="/readings">
          <CardContainer cards={cards}/>
